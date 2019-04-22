d7a31ffd9fbb71d0a8f632beafcfc85329f78f4d
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-04-22 16:57:57 -0300

    primeira view
    criada a primeira view

--- django/aula/todo/urls.py
+++ django/aula/todo/urls.py
@@ -12,10 +12,15 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+
+    este arquivo serve para dar nome as urls
 """
 from django.conf.urls import url
 from django.contrib import admin
 
+from todo.core import views
+
 urlpatterns = [
+	url(r'', views.home),
     url(r'^admin/', admin.site.urls),
 ]
