18c8fa6d8bcf3ba2bab808fe52fee269b8b71c80
Gustavo Grecco <gustavo.grecco@hotmail.com>
2019-04-22 11:38:12 -0300

    Create pessoa.py
    classe, objeto e construtor

---
+++ testes/pessoa.py
@@ -0,0 +1,19 @@
+class Pessoa:
+	nome = ""
+	idade = 0
+
+	def __init__(self, nome, idade):
+		self.nome = nome
+		self.idade = idade
+
+	def caminhar(self):
+		print("A Pessoa esta caminhando")
+
+	def parar(self):
+		print("A pessoa parou")
+
+p = Pessoa("Gustavo", 33)
+print("Nome: ", p.nome)
+print("idade: ", p.idade)
+p.caminhar()
+p.parar()
